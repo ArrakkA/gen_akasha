47b31720b34cb442596b306ea15c820ddc59290a
DESKTOP-S2C7CJG\computer <kys@greenit.cc>
2023-01-11 16:00:39 +0900

    완벽 등록

---
+++ src/main/resources/static/js/newArtifact.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+    optionMake();
+
+
+
+    $('#saveButton').click(function (){
+        const userName = $('#user_name').val();
+        const artifactCd = $('#artifact_name').val();
+        const artifactPart = $('#artifact_part').val();
+        const mainOption = $('#main_option').val();
+        const attack = $('#attack').val();
+        const attackPer = $('#attack_percent').val();
+        const defense = $('#defense').val();
+        const defensePer = $('#defense_percent').val();
+        const healthPoint = $('#health_point').val();
+        const healthPointPer = $('#health_point_percent').val();
+        const elementMastery = $('#element_mastery').val();
+        const elementCharge = $('#element_charge').val();
+        const criticalProbability = $('#critical_probability').val();
+        const criticalDamage = $('#critical_damage').val();
+
+        const params =JSON.stringify({
+            "userName" : userName
+            ,"artifactCd" : artifactCd
+            ,"artifactPart" : artifactPart
+            ,"mainOption" : mainOption
+            ,"attack" : attack
+            ,"attackPer": attackPer
+            ,"defense" : defense
+            ,"defensePer" : defensePer
+            ,"healthPoint" : healthPoint
+            ,"healthPointPer" : healthPointPer
+            ,"elementMastery" : elementMastery
+            ,"elementCharge" : elementCharge
+            ,"criticalProbability" : criticalProbability
+            ,"criticalDamage" : criticalDamage
+        });
+
+        console.log(params);
+
+        $.ajax({
+            type:'post'
+            ,url:'/v1/artifact/register'
+            ,data: params
+            ,contentType:'application/json'
+            ,dataType:'json'
+            ,success:function(data){
+                alert('등록되었습니다.');
+                document.location.reload();
+            }
+            ,error:function(xhr, error, msg){
+                console.log(xhr);
+                console.log(error);
+                console.log(msg);
+            }
+        });//ajax
+    })
+
+
+})
