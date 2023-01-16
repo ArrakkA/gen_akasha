ca53f603fe369756ed5c0d78e27440d53262b15b
DESKTOP-S2C7CJG\computer <kys@greenit.cc>
2023-01-16 17:00:09 +0900

    테스트 제조중 query dsl

--- src/test/java/play/genshin/akasha/domain/character/service/CharacterServiceImplTest.java
+++ src/test/java/play/genshin/akasha/domain/character/service/CharacterServiceImplTest.java
@@ -1,9 +1,18 @@
 package play.genshin.akasha.domain.character.service;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import play.genshin.akasha.domain.artifact.entity.Artifact;
+import play.genshin.akasha.domain.character.entity.EffectiveOption;
+import java.util.List;
+import static play.genshin.akasha.domain.character.entity.QEffectiveOption.*;
+
+import static play.genshin.akasha.domain.artifact.entity.QArtifact.*;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,11 +25,22 @@ class CharacterServiceImplTest {
 
     @Test
     public void re(){
-        //given
+        String userNameParam = "member1";
+        Integer ageParam = 10;
+
+        BooleanBuilder builder = new BooleanBuilder();
 
         //when
 
-        //then
-    }
+        Artifact artifact1 = queryFactory
+                .select(artifact)
+                .from(artifact).where(artifact.artifactNo.eq(1)).fetchOne();
 
+
+
+
+
+
+
+    }
 }
